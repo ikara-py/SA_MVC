3864a2c03ed5c69b8c18212b8b6db6cf1a2f9814
Ali Kara <ali16kara@gmail.com>
2026-01-14 09:25:59 +0100

    feat: Implement controller instantiation and method routing

--- app/core/App.php
+++ app/core/App.php
@@ -1,22 +1,38 @@
 <?php
 
 namespace App\Core;
+use App\Controllers\Home;
 
 class App
 {
-    protected $controller = 'home';
+    protected $controller = 'App\Controllers\Home';
     protected $method = 'index';
     protected $params = [];
     public function __construct()
     {
-        $url = $this->parseUrl();
+        // var_dump($_GET['url']);
+        $url = $this->parseUrl() ?? ['Home'];
 
-        var_dump( $url);
-        if(file_exists("../app/controllers/". $url[0] . ".php")){
-            $this->controller = $url[0];
+        // var_dump( $url);
+        if(file_exists("../app/controllers/{$url[0]}.php")){
+            $this->controller = 'App\Controllers\\' . $url[0];
             unset($url[0]);
         }
-        require_once "../app/controllers/" . $this->controller . ".php";
+        require_once "../app/controllers/" . basename(str_replace('\\', '/', $this->controller)) . ".php";
+        $this->controller = new $this->controller;
+        // var_dump($this->controller);
+        // echo gettype($this->controller);
+
+        if(isset($url[1])){
+            if(method_exists($this->controller, $url[1])){
+                $this->method = $url[1];
+                unset($url[1]);
+                // echo'does exist ???';
+            }
+        }
+        $this->params = $url ? array_values($url) : [];
+        // var_dump($this->params);
+        call_user_func_array([$this->controller, $this->method], $this->params);
     }
 
     public function parseUrl()
@@ -26,3 +42,4 @@ class App
         }
     }
 }
+
