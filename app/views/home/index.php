c07d87d79d11fb692d7831661cf79ff6b24c3ee2
ikara-py <ali16kara@gmail.com>
2026-01-15 11:15:26 +0100

    feat: Add admin, candidate, recruiter, home and error views

---
+++ app/views/home/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title><?= $title; ?></title>
+</head>
+<body>
+    <h1>Hello, <?= $user; ?>!</h1>
+    <p>Welcome to the TalentHub dashboard.</p>
+</body>
+</html>
