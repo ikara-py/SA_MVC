06eb4461c44ddef19d6b72747d4b56bf67c13fac
ikara-py <ali16kara@gmail.com>
2026-01-17 00:39:15 +0100

    feat: Update views with Twig templates and add auth pages

---
+++ app/views/errors/403.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>403 - Access Denied</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+    <div style="text-align: center; margin-top: 100px;">
+        <h1>403 - Access Denied</h1>
+        <p>You do not have the required permissions to view this dashboard.</p>
+        <a href="/AuthController/logout">Sign in with a different account</a>
+    </div>
+</body>
+</html>
